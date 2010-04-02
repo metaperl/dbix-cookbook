927337722f9ca60ab9a5de3ae3160f79b940ad34
Terrence Brannon <metaperl@locohost.(none)>
2010-04-02 16:56:57 -0400

    bogheads suggestions

--- lib/DBIx/Cookbook/DBIC/Command/custom_sql.pm
+++ lib/DBIx/Cookbook/DBIC/Command/custom_sql.pm
@@ -6,6 +6,26 @@ use Data::Dump;
 
 #use DBIx::Cookbook::DBIC::CustomSQL;
 
+f
+
+=for comment
+
+[16:47] <boghead> metaperl: look at the documentation for ->load_namespaces.  It allows you to specify custom resultset namespaces
+[16:47] <boghead> metaperl: You can also explicitly set your resultset in the result class
+[16:49] <boghead> metaperl: by the way, I applied your patch and checked it into the trunk
+[16:49] <boghead> metaperl++
+[16:49] <metaperl> ok, that's nice
+[16:50] <metaperl> by "in the Result class", those are auto-generated if I use ::Loader right? that's what I'm trying to avoid
+[16:51] <boghead> there are several ways to do it
+[16:52] <metaperl> well let's terminate this discussion as I'm leaving in 9 minutes
+[16:52] <boghead> you may be able to change your Schema::Loader config to force a different resultset even
+[16:52] <metaperl> hm
+[16:52] <boghead> or you could stick some custom code in the generated files, in the section allowing custom code
+[16:53] <metaperl> yes that's what I dont like doing at all :)
+[16:55] <boghead> default_resultset_class, and resultset_namespace parameters to load_namespaces are probably what you want then
+
+=cut
+
 sub execute {
   my ($self, $opt, $args) = @_;
 
