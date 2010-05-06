88c67238d5c07407c078b7499d4978eadf08737e
Terrence Brannon <metaperl@locohost.(none)>
2010-05-06 10:57:38 -0400

    ok skinny now runs fetch_all

--- lib/DBIx/Cookbook/Skinny/Sakila.pm
+++ lib/DBIx/Cookbook/Skinny/Sakila.pm
@@ -1,22 +1,22 @@
 package Sakila;
 
-our $config;
 
 BEGIN {
 
-  use DBIx::Cookbook::DBH;
-  $config = DBIx::Cookbook::DBH->new;
+  require DBIx::Cookbook::DBH;
+
+  my $config = DBIx::Cookbook::DBH->new;
+  my %setup = $config->for_skinny_setup;
+
+  use Data::Dumper;
+  warn 'setup: ', Dumper(\%setup);
+
+  require DBIx::Skinny;
+
+  DBIx::Skinny->import( setup => \%setup );
 
 }
 
-# use DBIx::Skinny::Schema setup => 
-my %setup = $config->for_skinny_setup;
-use Data::Dumper;
-warn 'setup: ', Dumper(\%setup);
-use DBIx::Skinny setup => 
-  {
-   %setup
-  };
 
 
 
