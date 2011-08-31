53c8c2fbe5dafd5e689f7c84ee150fe869a5919b
Terrence Brannon <metaperl@gmail.com>
2011-08-31 17:25:43 -0400

    examples

--- lib/DBIx/Cookbook/Custom/Command/grouping.pm
+++ lib/DBIx/Cookbook/Custom/Command/grouping.pm
@@ -9,16 +9,19 @@ sub execute {
     my ( $self, $opt, $args ) = @_;
 
     my $result = $self->connect->select(
-        table  => 'actor',
         column => [ 
 	  qw(first_name last_name),
-	  [ 'COUNT(film_actors.actor_id)' => 'film_count' ] ]
-    );
+	  [ 'COUNT(film_actors.actor_id)' => 'film_count' ] ],
+        table  => 'actor',
+	join => [
+	  'INNER JOIN film_actors USING (actor_id)'
+	 ]
+       );
 
     while ( my $row = $result->fetch_hash ) {
         warn $self->dumper($row);
     }
-}
+  }
 
 1;
 
